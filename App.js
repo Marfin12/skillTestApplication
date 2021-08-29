4252573ab270eb894ccb21e70831e3f8dd883cc5
Marfin Fadhilah <fadilahmarfin@gmail.com>
2021-08-29 23:14:09 +0700

    Push main apps

---
+++ App.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import {ApolloProvider} from 'react-apollo';
+
+import ApolloClient from 'apollo-boost';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import {Main, reducers} from './src';
+
+const client = new ApolloClient({
+  uri: 'http://192.168.8.114:4000/graphql',
+});
+
+const App = () => {
+  var middlewares = applyMiddleware(thunk);
+  const store = createStore(reducers, middlewares);
+
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Main isDarkMode />
+      </Provider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+};
+
+export default styles;
