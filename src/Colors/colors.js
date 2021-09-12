fbaf45d0cca27e466200ed9cefbba5c38d355a68
Marfin Fadhilah <fadilahmarfin@gmail.com>
2021-09-12 23:18:36 +0700

    feat(skillTestApplication): [Phase#5 - Add bottom-navigation] Change darkHeavyGray color for dark mode

--- src/Colors/colors.js
+++ src/Colors/colors.js
@@ -1,6 +1,6 @@
 const whitePrimary = '#FFF';
 const blackPrimary = '#000';
-const darkHeavyGray = '#111';
+const darkHeavyGray = '#222';
 const lightHeavyGray = '#EEE';
 const lightMediumGray = '#DDD';
 
